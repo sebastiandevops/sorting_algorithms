8da6ff2dd374f6f4d1d4e76c41f27e34e92158f7
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-21 15:58:58 -0500

    fixing

---
+++ 102-counting_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ *
+ * @array: array to be sorted.
+ * @size: Size of the array
+ *
+ * Return: void.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *output = (int *) malloc(sizeof(array[0]) * size);
+	int *count;
+	size_t max;
+	size_t i;
+
+	max = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > (int)max)
+		max = array[i];
+	}
+	count = (int *) malloc(sizeof(array[0]) * size);
+	for (i = 0; i <= max; ++i)
+		count[i] = 0;
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+	i = size - 1;
+	while (i != 0)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+		i--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+}
--- sort.h
+++ sort.h
@@ -31,5 +31,6 @@ void quick_sort(int *array, size_t size);
 void quickSort(int array[], int low, int high);
 int partition(int array[], int low, int high);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
