55f057380f92529836dcc016b2d7cf7912f86a90
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-21 22:37:38 -0500

    works but i dont know what happen with the checker

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -53,7 +53,7 @@ void countingSort(int array[], int size, int place)
 {
 	int *output;
 	int *count;
-	int max, i;
+	int max, i, k;
 
 	max = (array[0] / place) % 10;
 	output = (int *) malloc(sizeof(array[0]) * size + 1);
@@ -77,6 +77,78 @@ void countingSort(int array[], int size, int place)
 	}
 	for (i = 0; i < size; i++)
 		array[i] = output[i];
+	k = 0;
+	while (array && k < size)
+	{
+		if (k > 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		print_number(array[k]);
+		k++;
+	}
+	_putchar('\n');
 	free(count);
 	free(output);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
+/**
+ * print_number - function that prints an integer..
+ * @n: number of times that the diagonal will be printed.
+ *
+ *
+ * Return: void.
+ *
+ */
+void print_number(int n)
+{
+	int cont;
+	int aux;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		aux = n;
+		cont = -1;
+		while (aux < -9)
+		{
+			aux = (aux / 10);
+			cont = (cont * 10);
+		}
+		while (cont <= -1)
+		{
+			_putchar((n / cont) + '0');
+			n = (n % cont);
+			cont = (cont / 10);
+		}
+	}
+	else
+	{
+		aux = n;
+		cont = 1;
+		while (aux > 9)
+		{
+			aux = (aux / 10);
+			cont = (cont * 10);
+		}
+		while (cont >= 1)
+		{
+			_putchar((n / cont) + '0');
+			n = (n % cont);
+			cont = (cont / 10);
+		}
+	}
 }
