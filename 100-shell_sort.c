90ac785b4578ba9191cf20d0871e1da0e616ec23
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-10-21 21:54:24 -0500

    fixing

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -17,22 +17,25 @@ void shell_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	while (gap < size / 3)
+	if (size > 1)
 	{
-		gap = gap * 3 + 1;
-	}
-	while (gap > 0)
-	{
-		for (i = gap; i < size; i += 1)
+		while (gap < size / 3)
 		{
-			temp = array[i];
-			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
-			{
-				array[j] = array[j - gap];
-			}
-			array[j] = temp;
+			gap = gap * 3 + 1;
 		}
-		gap = (gap - 1) / 3;
+		while (gap > 0)
+		{
+			for (i = gap; i < size; i += 1)
+			{
+				temp = array[i];
+				for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
+				{
+					array[j] = array[j - gap];
+				}
+				array[j] = temp;
+			}
+			gap = (gap - 1) / 3;
+		}
 	}
 }
 
